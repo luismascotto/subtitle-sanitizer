c3647cdeca758dbf2f71a3c43bd96c85e6fd8800
Luis Mascotto <gustavomascotto@gmail.com>
2026-01-19 17:04:15 -0300

    helper bat

--- build.bat
+++ build.bat
@@ -1,1 +1,12 @@
+@echo off
+
+cd /d %~dp0
+
+echo Building subtitle-sanitizer...
 go build -o subtitle-sanitizer.exe ./cmd/sanitize
+
+if errorlevel 1 (
+    echo Failed to build subtitle-sanitizer.
+    exit /b 1
+)
+echo Done!
